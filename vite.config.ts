15214c4438dbb40fb50e20aa0fb656267ed2e5a4
keyurjivani1149 <keyurpatel1149@gmail.com>
2025-07-12 14:43:32 +0530

    completed work

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import fs from "fs";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
@@ -10,11 +11,20 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [
+ plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+    {
+      name: "copy-index-to-404",
+      closeBundle: () => {
+        const indexPath = path.resolve(__dirname, "dist/index.html");
+        const errorPath = path.resolve(__dirname, "dist/404.html");
+        if (fs.existsSync(indexPath)) {
+          fs.copyFileSync(indexPath, errorPath);
+          console.log("✅ 404.html created for GitHub Pages");
+        }
+      },
+    },
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
